1c51fd11dcef2ec2fae0c27c89270ce72680d990
vijendrajs <37953589+vijendrajs@users.noreply.github.com>
2025-02-15 16:19:22 +0530

    Case 4: test case using multiple number with "//;\n1;2"

--- add.py
+++ add.py
@@ -2,13 +2,20 @@ def add(numbers=''):
     numbers = str(numbers)
     if not numbers:
         return 0
-   
+    
     mainDelimeter = ','
+    if '//' in numbers:
+        # Find the positions of the delimiters
+        end_index = numbers.find('\n')
+        mainDelimeter = numbers[2:end_index]
+        numbers = numbers.replace('//', '')
+
     delimeterList = [',', '\n', '?']
+    delimeterList.append(mainDelimeter)
     if any(delimiter in numbers for delimiter in delimeterList):
         for delimiter in delimeterList:
             numbers = numbers.replace(delimiter, ',')
-        numbersList = numbers.split(mainDelimeter)
+        numbersList = [int(num) for num in numbers.split(',') if num]
         numbersList = list(map(int, numbersList))
         result = sum(numbersList)
         return result
@@ -43,4 +50,12 @@ print('result', result)
 result = add('1\n2,3,4\n5?6')
 print('result', result)
 result = add('1\n2,3,4\n5?6?7')
+print('result', result)
+
+#Dynamic delimeter at starting in string - //;\n
+result = add('//;\n1;2')
+print('result', result)
+result = add('//;;\n1;;2')
+print('result', result)
+result = add('//;;\n1;;2;;,3\n3')
 print('result', result)
