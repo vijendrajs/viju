927bb92f8d433fee27c1058ce868f8c19e9c524f
vijendrajs <37953589+vijendrajs@users.noreply.github.com>
2025-02-15 15:59:59 +0530

    #Case 2: test cases using string multiple number in a string comma saperated

--- add.py
+++ add.py
@@ -1,11 +1,16 @@
-#Case 1: Test Code for empty string and "1" string and numeric 1 string.
-
 def add(numbers=''):
     numbers = str(numbers)
     if not numbers:
         return 0
     else:    
-        return numbers
+        delimeter = ','
+        if delimeter in numbers:
+            numbersList = numbers.split(delimeter)
+            numbersList = list(map(int, numbersList))
+            result = sum(numbersList)
+            return result
+        else:
+            return numbers
 
 #Case: Empty String
 result = add("")
@@ -17,4 +22,10 @@ print('result', result)
 
 #Case: Single Number in numeric format
 result = add(1)
+print('result', result)
+
+#Case: multiple Number
+result = add('1,2')
+print('result', result)
+result = add('1,2,5,6')
 print('result', result)
